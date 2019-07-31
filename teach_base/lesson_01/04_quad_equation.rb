e427aafbe1b41716afff7d358215b7da2ddd94eb
vetolmedzol <vetolmedzol@gmail.com>
2019-07-31 15:31:44 +0300

    Teach_Base Lesson_01 Task_04

--- teach_base/lesson_01/04_quad_equation.rb
+++ teach_base/lesson_01/04_quad_equation.rb
@@ -1,26 +1,34 @@
-=begin
-Квадратное уравнение. Пользователь вводит 3 коэффициента a, b и с.
-Программа вычисляет дискриминант (D) и корни уравнения (x1 и x2, если они есть) 
-и выводит значения дискриминанта и корней на экран. При этом возможны следующие варианты:
-Если D > 0, то выводим дискриминант и 2 корня
-Если D = 0, то выводим дискриминант и 1 корень (т.к. они в этом случае равны)
-Если D < 0, то выводим дискриминант и сообщение "Корней нет"
-=end
+# frozen_string_literal: true
 
-puts "Enter 3 coeff!"
-a = gets.chomp.to_f
-b = gets.chomp.to_f
-c = gets.chomp.to_f
+# Quadratic equation. The user enters 3 coefficients a, b and c.
+# The program calculates the discriminant (D) and the roots
+# of the equation (x1 and x2, if any) and displays the discriminant
+# values and roots on the screen. The following options are possible:
+# If D> 0, then we deduce the discriminant and 2 roots
+# If D = 0, then we derive the discriminant and 1 root (since in this
+# case they are equal)
+# If D <0, then we display the discriminant and the message "No roots"
 
-def result (a, b, c)
-  dis = b**2 - 4 * a * c
-  if dis < 0
-    "Discriminant is #{dis}. No roots!"
-  elsif dis == 0
-    "Discriminant is #{dis}. And root is #{-b/(a * 2)}!"
+puts 'Enter 3 coeff!'
+coef1 = gets.chomp.to_f
+coef2 = gets.chomp.to_f
+coef3 = gets.chomp.to_f
+discriminant = coef2**2 - 4 * coef1 * coef3
+puts "Discriminant is #{discriminant}."
+
+def result(coef1, coef2, coef3, discriminant)
+  if discriminant.positive?
+    discriminant_positive(coef1, coef2, coef3, discriminant)
+  elsif discriminant.zero?
+    "x1 = x2 = #{- coef2 / (coef1 * 2)}"
   else
-    "Discriminant is #{dis}. With roots #{(-b - Math.sqrt(dis))/(a * 2)} and #{(-b - Math.sqrt(dis))/(a * 2)}!"
+    'No roots!'
   end
 end
 
-puts result(a, b, c)
+def discriminant_positive(coef1, coef2, _coef3, discriminant)
+  "x1 = #{(- coef2 + Math.sqrt(discriminant)) / (2 * coef1)} \
+  x2 = #{(- coef2 - Math.sqrt(discriminant)) / (coef1 * 2)}"
+end
+
+puts result(coef1, coef2, coef3, discriminant)
