4a0ed647eefe7b1f08033ce7db140fbb5be64aec
vetolmedzol <vetolmedzol@gmail.com>
2019-07-31 15:29:46 +0300

    Teach_Base Lesson_01 Task_01

--- teach_base/lesson_01/01_ideal_weight.rb
+++ teach_base/lesson_01/01_ideal_weight.rb
@@ -1,18 +1,20 @@
-=begin
-	Идеальный вес. Программа запрашивает у пользователя имя и рост и выводит 
-	идеальный вес по формуле <рост> - 110, после чего выводит результат пользователю 
-	на экран с обращением по имени. Если идеальный вес получается отрицательным,
-	 то выводится строка "Ваш вес уже оптимальный"
-=end
+# frozen_string_literal: true
 
-puts "Hi, what is your name?"
+# Perfect weight. The program prompts the user for the name
+# and height and displays the ideal weight using the formula
+# <height> - 110, after which it displays the result to the
+# user on the screen with the name reference. If the ideal
+# weight is negative, then the line 'Your weight is already
+# optimal' is displayed.
+
+puts 'Hi, what is your name?'
 name = gets.chomp
 
 puts "#{name}, what is your height, cm?"
 height = gets.chomp.to_i
 
-if height - 110 < 0
+if (height - 110).negative?
   puts "#{name}, your weight is ideal now!"
-else 
+else
   puts "#{name}, your ideal weight is #{height - 110}!"
 end
