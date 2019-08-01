98d52dd71b23c77859c2163d2292a7115effd46f
vetolmedzol <vetolmedzol@gmail.com>
2019-08-01 11:23:50 +0300

    TeachBase Lesson_01 Task_01

--- teach_base/lesson_02/01_30_days_months.rb
+++ teach_base/lesson_02/01_30_days_months.rb
@@ -1,21 +1,21 @@
-=begin
-Сделать хеш, содеращий месяцы и количество дней в месяце. 
-В цикле выводить те месяцы, у которых количество дней ровно 30
-=end
+# frozen_string_literal: true
+
+# Make hash containing months and number of days in a month.
+# In the cycle, output those months in which the number of days is exactly 30.
 
 months = {
-  January: 31,
-  February: 28,
-  March: 31,
-  April: 30,
-  May:  31,
-  June: 30,
-  July: 31,
-  August: 31,
-  September: 30,
-  October: 31,
-  November:30,
-  December: 31
+  january: 31,
+  february: 28,
+  march: 31,
+  april: 30,
+  may: 31,
+  june: 30,
+  july: 31,
+  august: 31,
+  september: 30,
+  october: 31,
+  november: 30,
+  december: 31
 }
 
 months.each { |key, value| puts key if value == 30 }
