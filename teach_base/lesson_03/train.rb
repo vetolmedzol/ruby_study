b396f9ce197ad830584aff095eeb9b5fa40c39c5
vetolmedzol <vetolmedzol@gmail.com>
2019-08-13 15:14:20 +0300

    TeachBase Lesson_03 train.rb

--- teach_base/lesson_03/train.rb
+++ teach_base/lesson_03/train.rb
@@ -15,19 +15,33 @@
 # Can take a route (object of class Route)
 # It can move between stations indicated in the route.
 # Show previous station, current, next, based on route
+require_relative 'station.rb'
+require_relative 'route.rb'
 class Train
   attr_accessor :speed, :id, :number_of_cars, :train_type
 
-  def initialize(id, train_type, number_of_cars = 5)
+  def initialize(id, train_type, route, number_of_cars = 5)
     @id = id
-    @train_type = train_type
+    train_type = :passenger unless correct_train_type?(train_type)
+    @train_type = train_type.to_sym
     @number_of_cars = number_of_cars
     @speed = 0
     @position = 0
+    @route = route
   end
 
-  @prev_position = proc { route.route_array[@position - 1].positive? }
-  @next_position = proc { route.route_array[@position].positive? }
+  def correct_train_type?(train_type)
+    train_type_arr = [:cargo, :passenger]
+    train_type_arr.include?(train_type.to_sym)
+  end
+
+  def prev_position?
+    !@route.route_array[@position - 1].nil?
+  end
+
+  def next_position?
+    !@route.route_array[@position].nil?
+  end
 
   def current_speed
     @speed
@@ -53,20 +67,21 @@ class Train
     self.number_of_cars -= 1 if speed.zero? && number_of_cars.positive?
   end
 
-  def current_position(route)
-    "Previous station #{route.route_array[@position - 1]}" if @prev_position
-    "Next station is #{route.route_array[@position + 1]}" if @next_position
-    "Current station #{route.route_array[@position]}"
+  def current_position
+    "Previous station #{@route.route_array[@position - 1]}" if prev_position?
+    "Next station is #{@route.route_array[@position + 1]}" if next_position?
+    "Current station #{@route.route_array[@position]}"
   end
 
-  def move_forward(route)
-    @position += 1 unless route.route_array[@position + 1].nil?
-    route.route_array[@position]
-  end
-
-  def move_back(route)
-    @position -= 1 unless route.route_array[@position - 1].nil?
-    route.route_array[@position]
+  def move_forward
+    if @route.route_array[@position] == @route.route_array.last
+      'You at the end!'
+    else
+      @position += 1 unless @route.route_array[@position + 1].nil?
+      @route.route_array[@position - 1].send_train(self)
+      @route.route_array[@position].add_train(self)
+      @route.route_array[@position]
+    end
   end
 
   def to_s
