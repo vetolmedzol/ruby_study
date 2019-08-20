952a07dd39aeff66849be59f10fca15129949a79
vetolmedzol <vetolmedzol@gmail.com>
2019-08-20 13:34:48 +0300

    TeachBase Lesson_07 Proc and lambda

---
+++ teach_base/lesson_07/passenger_car.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Class PassengerCar
+class PassengerCar < Car
+  include Company
+  attr_accessor :capacity, :seating
+  attr_reader :car_id
+
+  def initialize(seating = 30)
+    @car_id = object_id
+    @capacity = seating.to_i
+    @seating = seating.to_i
+    validate!
+  end
+
+  def take_place
+    self.seating -= param if @seating.positive?
+  end
+
+  def free_places
+    seating
+  end
+
+  def occupied_places
+    @capacity - @seating
+  end
+
+  def to_s
+    "car #{@car_id}"
+  end
+end
